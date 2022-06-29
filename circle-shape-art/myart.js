570f07fc4364d6dbc5f9ff10c1ea73ad11235ed8
IyLias <iliaskyrin9707@gmail.com>
2022-06-30 00:02:58 +0900

    finished this circle shape art project

--- circle-shape-art/myart.js
+++ circle-shape-art/myart.js
@@ -10,8 +10,8 @@ const SCREEN_HEIGHT = 700;
 let myCircle,myRect,myRect2;
 let myBackground;
 
-let artist;
 let myEllipseCollection;
+
 
 // ---------------------------- main function -----------------------------------
 
@@ -20,57 +20,15 @@ function setup(){
     myBackground = new Canvas('#ffffee',SCREEN_WIDTH,SCREEN_HEIGHT);
     myBackground.drawBackground();
 
-    artist = new Artist();
-    
-    myEllipseCollection = new MyEllipseCollection(10);
-
-
-    myCircle = new MyEllipse('#F52F11',100,20,50,100);
-    myCircle.setStroke(true,'#5500F9',4);
-    myCircle.setFill(false);
-
-   
-    myRect2 = new MyRect('#111111',-150,30,100,50);
-    myRect2.setStroke(true,'#111111',5);
-    myRect2.setFill(false);
+    myEllipseCollection = new MyEllipseCollection(30);
 
 }
 
 function draw(){
 
+    myEllipseCollection.draw();
     
-    myCircle.draw();
-   
-    myRect2.draw();
 }
 
 // -----------------------------------------------------------------------------
 
-
-
-
-// class definition
-
-
-// MyEllipse set for drawing lots of ellipses
-class MyEllipseCollection{
-
-    constructor(ellipseNum){
-        this.myEllipses = [];        
-        this.ellipseNum = ellipseNum;
-        for(var i=0;i<this.ellipseNum;i++){
-            let myEllipse = new MyEllipse();
-            this.myEllipses.push(myEllipse);
-        }
-
-    }
-
-
-
-    draw(){
-        for(var i=0;i<this.myEllipses.length;i++)
-            this.myEllipses[i].draw();
-    }
-
-
-}
--- circle-shape-art/shape.js
+++ circle-shape-art/shape.js
@@ -9,7 +9,7 @@ class Canvas{
         this.color = color;
         this.width = w;
         this.height = h;
-        createCanvas(this.width,this.height,WEBGL);
+        createCanvas(this.width,this.height);
     }
 
 
@@ -31,7 +31,7 @@ class Artist{
     }
 
 
-
+    
 
 
 
@@ -119,6 +119,89 @@ class MyEllipse extends D2Shape{
         super.draw();
         ellipse(this.x,this.y,this.w,this.h);
     }
+}
+
+
+// MyEllipse set for drawing lots of ellipses
+class MyEllipseCollection{
+
+    constructor(ellipseNum){
+        this.myEllipses = [];        
+        this.ellipseNum = ellipseNum;
+        for(var i=0;i<this.ellipseNum;i++){
+            let myColor = this.generateColor();
+            let [myX,myY,myW,myH] = this.generatePositionAndSize();
+            console.log(typeof(myX),typeof(myY));
+            let myEllipse = new MyEllipse(myColor,myX,myY,myW,myH);
+
+            // need to set all properties
+            // color, x,y,w,h
+            // isStroke, stroke, strokeWeight, isFill
+            // isStroke and isFill can't be both false
+            let [isStroke,strokeColor,strokeWeight,isFill] = this.generateStrokeAndFill();
+            myEllipse.setStroke(isStroke,strokeColor,strokeWeight);
+            myEllipse.setFill(isFill);
+
+            this.myEllipses.push(myEllipse);
+        }
+
+    }
+
+    // generate random color
+    generateColor(){
+        const unit = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
+        const length = 6;
+        let color = '#';
+
+        for(var i=0;i<length;i++)
+            color += unit[Math.floor(Math.random()*16)];
+
+        return color;
+    }
+
+    // generate position and size
+    generatePositionAndSize(){
+        let x,y;
+        x = Math.floor(Math.random()*SCREEN_WIDTH);
+        y = Math.floor(Math.random()*SCREEN_HEIGHT);
+
+        let w,h;
+        const alpha = 0.1;
+        const sizeSeed = 300;
+        w = Math.ceil((Math.random()+alpha)*sizeSeed);
+        h = Math.ceil((Math.random()+alpha)*sizeSeed);
+        
+        console.log(x,y,w,h);
+        return [x,y,w,h];
+    }
+
+    // generate stroke and fill values
+    generateStrokeAndFill(){
+        const WEIGHTMAX = 10;
+        const alpha = 0.1;
+
+        let isStroke,strokeColor,strokeWeight,isFill;
+        isStroke = (Math.floor(Math.random()*100))%2 ? true : false;
+        if(isStroke === false){
+            strokeColor = 0, strokeWeight = 0;
+            isFill = true;
+        }else{
+            strokeColor = this.generateColor();
+            strokeWeight = Math.floor((Math.random()+alpha)*WEIGHTMAX);
+            isFill = (Math.floor(Math.random()*100))%2 ? true : false;
+        }
+
+        return [isStroke,strokeColor,strokeWeight,isFill];
+    }
+
+
+
+    draw(){
+        for(var i=0;i<this.myEllipses.length;i++)
+            this.myEllipses[i].draw();
+    }
+
+
 }
 
 
